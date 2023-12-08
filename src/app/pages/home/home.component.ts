d5aef301dc244a5704ad0db3a566e4a0d12114cf
jp <jpespechit@gmail.com>
2023-12-08 13:09:11 -0300

    layout finished

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { BlogTitleComponent } from "../../components/blog-title/blog-title.component";
 import { FeaturedPostComponent } from "../../components/featured-post/featured-post.component";
+import { OtherPostComponent } from "../../components/other-post/other-post.component";
+import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [BlogTitleComponent, FeaturedPostComponent]
+    imports: [BlogTitleComponent, FeaturedPostComponent, OtherPostComponent, NavBarComponent]
 })
 export class HomeComponent {
 
